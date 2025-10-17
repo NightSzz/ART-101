127d0aa3b56063824801eedb55a9f3bec67a1e32
TheMoonEnjoyer <lunatelles66@gmail.com>
2025-10-17 16:14:53 -0700

    supeerdupperupdate

--- environ/env03.js
+++ environ/env03.js
@@ -1,5 +1,8 @@
 //Add a div with an ID to your html, similar to <div id="output"></div> from the follow along.
+
 //Use $("#output").append( ..... ); to add content to this div. Observe how ID is used!
+$("#output").append("<h2> and then...</h2>");
+$("#output").append("<p> the mango grew to be known as the best warrior mango in the kingdom of mangoes. </p>");
 //You can use multiple DIVs with different IDs for different bits of content.
 //Observe how HTML tags are inserted into JS variables before they are printed. Use it!
 //You can also CSS-style those DIVs using the same IDs for CSS selectors!
