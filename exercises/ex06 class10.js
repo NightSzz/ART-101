60d382bfc7e6b483dd2086a0c21f8b225a3fbbe3
TheMoonEnjoyer <lunatelles66@gmail.com>
2025-10-30 18:33:44 -0700

    ...

---
+++ exercises/ex06 class10.js
@@ -0,0 +1,66 @@
+var count = 0;
+let colorCount = 0;
+var colors = ["Orchid", "Coral", "HotPink", "Plum"];
+//images 
+function makeImage(imageName) {
+     if (colors[colorCount] == imageName) {
+     $("body").append("<img width=50 src='images/" + imageName + ".png'>");
+    }
+}
+//background color
+function changeBackground (newColor) {
+     $("body").css("background-color", newColor);
+}
+$(".color-button").click(function () {
+
+    changeBackground(this.id);
+});
+
+function moody(moodyCount) {
+    let mood = "";
+    if (moodyCount < 5) { mood = "fresh and happy"; }
+    else if ((moodyCount >= 5) && (moodyCount < 10)) { mood = "keep pushing"; }
+    else { mood = "so tired"; }
+
+    return mood;
+}
+
+
+
+//the button
+$("#needy-button").click(function () {
+//moved the mood code into a function
+     //if (count < 5) { mood = "fresh and happy"; }
+     //else if ((count >= 5) && (count < 10)) { mood = "keep pushing"; }
+     //else { mood = "so tired"; }
+   let moodMessage = moody(count);
+
+   $("#needy-button").html("Clicks: " + count + " Color: " + colors[colorCount] + " " + moodMessage);
+    //unused code>>>
+     //$("body").css("background-color", colors[colorCount]); 
+     // if (colors[colorCount] == "Orchid") {
+     // $("body").append("<img width=50 src='images/orchid.png'>");
+   
+    changeBackground(colors[colorCount]);
+
+     makeImage("Orchid");
+     makeImage("Coral");
+     makeImage("Plum");
+     makeImage("HotPink"); 
+
+   count = count + 1;
+   colorCount = colorCount + 1;
+   if (colorCount == 4) { colorCount = 0; }
+});
+
+//audio play/pause
+const myAudio = document.getElementById('myAudio');
+const playPauseButton = document.getElementById('playPauseButton');
+
+playPauseButton.addEventListener('click', function() {
+    if (myAudio.paused) {
+        myAudio.play();
+    } else {
+        myAudio.pause();
+    }
+});
